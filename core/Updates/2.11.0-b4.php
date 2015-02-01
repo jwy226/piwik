de9ad1b53963dcc92ad5555ece86742ca7123125
Matthieu Napoli <matthieu@mnapoli.fr>
2015-02-02 11:45:32 +1300

    Moved the monolog configuration into a Monolog plugin

---
+++ core/Updates/2.11.0-b4.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Updates;
+
+use Piwik\Plugin\Manager;
+use Piwik\Updates;
+
+class Updates_2_11_0_b4 extends Updates
+{
+    static function update()
+    {
+        Manager::getInstance()->activatePlugin('Monolog');
+    }
+}
